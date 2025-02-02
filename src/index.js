6907c9da5dcf73c26ba234248212f9547f268ebe
ParasP41 <parasvishwakarma198@gmail.com>
2025-02-02 15:32:24 +0530

    exp

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// require("dotenv").config({path:"./env"})
+
+import dotenv from "dotenv";
+import connectDB from "../src/db/index.js";
+dotenv.config({path: "./env"})
+connectDB();
+
+
+
+
+/* In this method of all code of db connection is in the index.js file 
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants";
+import express from "express";
+const app=express();
+; (async () => {
+try{
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error", (err) => {
+        console.log("Express is not working properly")
+        throw err
+    })
+    app.listen(process.env.PORT,()=>
+    {
+        console.log("Server is running on port :",process.env.PORT)
+    })
+}catch(e){
+    console.error("Error : "+ERROR)
+    throw err
+}
+})()
+*/
